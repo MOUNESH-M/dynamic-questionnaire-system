026474a5c5c884488d40b6a3761bdf369c436f0b
Mounesh M <mounesh96290@gmail.com>
2026-06-12 20:04:39 +0530

    created authentication module structure

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,27 @@
 from fastapi import FastAPI
+from app.core.database import database
 
 app=FastAPI(
     title="Dynamic Questionnaire System",
     version="1.0.0"
 )
 
-@app.get("/health")
-def health_check():
+@app.get("/health", tags=["Health"])
+def health_check()->dict:
+    #Application health check.
     return{"status":"UP"}
+
+@app.get("/health/db", tags=["Health"])
+def database_health_check() -> dict:
+    try:
+        database.command("ping")
+        return {
+            "status": "UP",
+            "database": "CONNECTED"
+        }
+    except Exception as e:
+        return {
+            "status": "DOWN",
+            "database": "DISCONNECTED",
+            "error": str(e)
+        }
